cfaaf41c1ae026472a93637b053bcc0f7a17ac47
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2024-05-05 23:02:18 +0200

    Stuff

--- apps/lsp/src/main.rs
+++ apps/lsp/src/main.rs
@@ -10,7 +10,57 @@ struct Backend {
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                position_encoding: None,
+                text_document_sync: None,
+                selection_range_provider: None,
+                hover_provider: None,
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: None,
+                    trigger_characters: None,
+                    all_commit_characters: None,
+                    work_done_progress_options: WorkDoneProgressOptions {
+                        work_done_progress: None,
+                    },
+                    completion_item: Some(CompletionOptionsCompletionItem {
+                        label_details_support: Some(true),
+                    }),
+                }),
+                signature_help_provider: None,
+                definition_provider: None,
+                type_definition_provider: None,
+                implementation_provider: None,
+                references_provider: None,
+                document_highlight_provider: None,
+                document_symbol_provider: None,
+                workspace_symbol_provider: None,
+                code_action_provider: None,
+                code_lens_provider: None,
+                document_formatting_provider: None,
+                document_range_formatting_provider: None,
+                document_on_type_formatting_provider: None,
+                rename_provider: None,
+                document_link_provider: None,
+                color_provider: None,
+                folding_range_provider: None,
+                declaration_provider: None,
+                execute_command_provider: None,
+                workspace: None,
+                call_hierarchy_provider: None,
+                semantic_tokens_provider: None,
+                moniker_provider: None,
+                linked_editing_range_provider: None,
+                inline_value_provider: None,
+                inlay_hint_provider: None,
+                diagnostic_provider: None,
+                experimental: None,
+            },
+            server_info: Some(ServerInfo {
+                name: "code_analyzer".to_string(),
+                version: Some("0.1.0".to_string()),
+            }),
+        })
     }
 
     async fn initialized(&self, _: InitializedParams) {
@@ -21,6 +71,29 @@ impl LanguageServer for Backend {
 
     async fn shutdown(&self) -> Result<()> {
         Ok(())
+    }
+
+    async fn completion(&self, _params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![CompletionItem {
+            label: "popopop".to_string(),
+            label_details: None,
+            kind: None,
+            detail: None,
+            documentation: None,
+            deprecated: None,
+            preselect: None,
+            sort_text: None,
+            filter_text: None,
+            insert_text: None,
+            insert_text_format: None,
+            insert_text_mode: None,
+            text_edit: None,
+            additional_text_edits: None,
+            command: None,
+            commit_characters: None,
+            data: None,
+            tags: None,
+        }])))
     }
 }
 
--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,5 +1,6 @@
 import './class_diagram.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <div><Component {...pageProps} /><Analytics /></div>;
 }
